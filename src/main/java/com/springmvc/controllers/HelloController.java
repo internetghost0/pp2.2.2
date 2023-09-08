e33c9a650f6ca3bef7b61d6df7f0d326dba0ed0d
zer0 <internetghost0@protonmail.com>
2023-09-08 18:25:30 +0200

    init

---
+++ src/main/java/com/springmvc/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package com.springmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String index() {
+        return "index";
+    }
+}
